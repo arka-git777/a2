528d95863322e6eff190569b79d6e7ba4b5bb219
arkad <arkad@ARKADII>
2025-02-16 23:08:22 +0100

    Add project files.

---
+++ a2/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace a2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            dateoutput = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            btnNext = new Button();
+            btnPrevious = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(483, 94);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(211, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(713, 94);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "submit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dateoutput
+            // 
+            dateoutput.AutoSize = true;
+            dateoutput.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dateoutput.Location = new Point(103, 43);
+            dateoutput.Name = "dateoutput";
+            dateoutput.Size = new Size(0, 25);
+            dateoutput.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(12, 94);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(232, 93);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "submit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(254, 210);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(221, 111);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // btnNext
+            // 
+            btnNext.Location = new Point(540, 254);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(75, 23);
+            btnNext.TabIndex = 6;
+            btnNext.Text = "next";
+            btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += btnNext_Click_1;
+            // 
+            // btnPrevious
+            // 
+            btnPrevious.Location = new Point(124, 254);
+            btnPrevious.Name = "btnPrevious";
+            btnPrevious.Size = new Size(75, 23);
+            btnPrevious.TabIndex = 7;
+            btnPrevious.Text = "previous";
+            btnPrevious.UseVisualStyleBackColor = true;
+            btnPrevious.Click += btnPrevious_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnPrevious);
+            Controls.Add(btnNext);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(dateoutput);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Label dateoutput;
+        private DateTimePicker dateTimePicker1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private Button btnNext;
+        private Button btnPrevious;
+    }
+}
---
+++ a2/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System.Globalization;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace a2
+{
+    public partial class Form1 : Form
+    {
+        private List<string> imagePaths; // Список шляхів до зображень
+        private int currentImageIndex = 0; // Поточний індекс зображення
+        public Form1()
+        {
+            InitializeComponent();
+            LoadImages();
+            ShowCurrentImage();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputDate = textBox1.Text;
+
+            if (DateTime.TryParse(inputDate, out DateTime date))
+            {
+                string dayOfWeek = date.ToString("dddd", new CultureInfo("en"));
+                dateoutput.Text = dayOfWeek;
+            }
+            else
+            {
+                dateoutput.Text = "Error!!!";
+            }
+
+
+            if (DateTime.TryParse(textBox1.Text, out DateTime targetDate))
+            {
+                DateTime now = DateTime.Now;
+
+                if (targetDate > now)
+                {
+                    int years = targetDate.Year - now.Year;
+                    int months = targetDate.Month - now.Month;
+                    int days = targetDate.Day - now.Day;
+                    int hours = targetDate.Hour - now.Hour;
+                    int minutes = targetDate.Minute - now.Minute;
+                    int seconds = targetDate.Second - now.Second;
+
+                    if (seconds < 0)
+                    {
+                        seconds += 60;
+                        minutes--;
+                    }
+                    if (minutes < 0)
+                    {
+                        minutes += 60;
+                        hours--;
+                    }
+                    if (hours < 0)
+                    {
+                        hours += 24;
+                        days--;
+                    }
+                    if (days < 0)
+                    {
+                        days += DateTime.DaysInMonth(now.Year, now.Month);
+                        months--;
+                    }
+                    if (months < 0)
+                    {
+                        months += 12;
+                        years--;
+                    }
+
+                    dateoutput.Text = $"{years} years, {months} months, {days} days, {hours} hours, {minutes} minutes, {seconds} seconds";
+                }
+                else
+                {
+                    dateoutput.Text = "Date is in the past";
+                }
+            }
+            else
+            {
+                dateoutput.Text = "Error!!!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime targetDate = dateTimePicker1.Value;
+            DateTime now = DateTime.Now;
+
+            if (targetDate > now)
+            {
+                int years = targetDate.Year - now.Year;
+                int months = targetDate.Month - now.Month;
+                int days = targetDate.Day - now.Day - 1;
+                int hours = 24 - now.Hour;
+                int minutes = 60 - now.Minute;
+                int seconds = 60 - now.Second;
+                if (seconds < 0)
+                {
+                    seconds += 60;
+                    minutes--;
+                }
+                if (minutes < 0)
+                {
+                    minutes += 60;
+                    hours--;
+                }
+                if (hours < 0)
+                {
+                    hours += 24;
+                    days--;
+                }
+                if (days < 0)
+                {
+                    days += DateTime.DaysInMonth(now.Year, now.Month);
+                    months--;
+                }
+                if (months < 0)
+                {
+                    months += 12;
+                    years--;
+                }
+
+                dateoutput.Text = $"{years} years, {months} months, {days} days, {hours} hours, {minutes} minutes, {seconds} seconds left";
+            }
+            else
+            {
+                dateoutput.Text = "Date is in the past";
+            }
+        }
+        private void LoadImages()
+        {
+            try
+            {
+                string imageFolder = @"C:\Users\arkad\OneDrive\Рабочий стол\Images";
+                imagePaths = new List<string>(Directory.GetFiles(imageFolder, "*.jpg", SearchOption.TopDirectoryOnly));
+
+                if (imagePaths.Count == 0)
+                {
+                    MessageBox.Show("There is no images with jpg");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ShowCurrentImage()
+        {
+            if (imagePaths.Count > 0)
+            {
+                pictureBox1.Image = Image.FromFile(imagePaths[currentImageIndex]);
+            }
+        }
+
+
+        private void btnNext_Click_1(object sender, EventArgs e)
+        {
+            if (imagePaths.Count == 0) return;
+
+            currentImageIndex = (currentImageIndex + 1) % imagePaths.Count;
+            ShowCurrentImage();
+        }
+
+        private void btnPrevious_Click_1(object sender, EventArgs e)
+        {
+            if (imagePaths.Count == 0) return;
+
+            currentImageIndex = (currentImageIndex - 1 + imagePaths.Count) % imagePaths.Count;
+            ShowCurrentImage();
+        }
+    }
+
+}
+
